8535ad967b0cbd4fec5606dbf6428174d0892051
DylanPatrascu <dylanpatrascu1@gmail.com>
2025-05-24 23:59:56 -0400

    Created project

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float locomotionBlendSpeed = 0.02f;
+
+    private PlayerLocomotionInput _playerLocomotionInput;
+    private PlayerState _playerState;
+    private PlayerController _playerController;
+
+    private static int inputXHash = Animator.StringToHash("InputX");
+    private static int inputYHash = Animator.StringToHash("InputY");
+    private static int inputMagnitudeHash = Animator.StringToHash("InputMagnitude");
+    private static int isIdlingHash = Animator.StringToHash("IsIdling");
+    private static int isGroundedHash = Animator.StringToHash("IsGrounded");
+    private static int isFallingHash = Animator.StringToHash("IsFalling");
+    private static int isJumpingHash = Animator.StringToHash("IsJumping");
+    private static int isRotatingToTargetHash = Animator.StringToHash("IsRotatingToTarget");
+    private static int rotationMismatchHash = Animator.StringToHash("RotationMismatch");
+
+    private Vector3 _currentBlendInput = Vector3.zero;
+
+    private void Awake()
+    {
+        _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        _playerState = GetComponent<PlayerState>();
+        _playerController = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        bool isIdling = _playerState.CurrentPlayerMovementState == PlayerMovementState.Idling;
+        bool isRunning = _playerState.CurrentPlayerMovementState == PlayerMovementState.Running;
+        bool isSprinting = _playerState.CurrentPlayerMovementState == PlayerMovementState.Sprinting;
+        bool isJumping = _playerState.CurrentPlayerMovementState == PlayerMovementState.Jumping;
+        bool isFalling = _playerState.CurrentPlayerMovementState == PlayerMovementState.Falling;
+        bool isGrounded = _playerState.InGroundedState();
+
+        Vector2 inputTarget = isSprinting ? _playerLocomotionInput.MovementInput * 1.5f : isRunning ? _playerLocomotionInput.MovementInput * 1f : _playerLocomotionInput.MovementInput * 0.5f;
+        _currentBlendInput = Vector3.Lerp(_currentBlendInput, inputTarget, locomotionBlendSpeed * Time.deltaTime);
+
+        _animator.SetBool(isIdlingHash, isIdling);
+        _animator.SetBool(isGroundedHash, isGrounded);
+        _animator.SetBool(isFallingHash, isFalling);
+        _animator.SetBool(isJumpingHash, isJumping);
+        _animator.SetBool(isRotatingToTargetHash, _playerController.IsRotatingToTarget);
+        _animator.SetFloat(inputXHash, _currentBlendInput.x);
+        _animator.SetFloat(inputYHash, _currentBlendInput.y);
+        _animator.SetFloat(inputMagnitudeHash, _currentBlendInput.magnitude);
+        _animator.SetFloat(rotationMismatchHash, _playerController.RotationMismatch);
+    }
+
+}
---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public enum PlayerMovementState
+{
+    Idling = 0,
+    Walking = 1,
+    Running = 2,
+    Sprinting = 3,
+    Jumping = 4,
+    Falling = 5,
+    Strafing = 6,
+}
+public class PlayerState : MonoBehaviour
+{
+    [field: SerializeField] public PlayerMovementState CurrentPlayerMovementState { get; private set; } = PlayerMovementState.Idling;
+    public void SetPlayerMovementState(PlayerMovementState playerMovementState)
+    {
+        CurrentPlayerMovementState = playerMovementState;
+    }
+
+    public bool InGroundedState()
+    {
+        return IsStateGroundedState(CurrentPlayerMovementState);
+    }
+
+    public bool IsStateGroundedState(PlayerMovementState movementState)
+    {
+        return CurrentPlayerMovementState == PlayerMovementState.Idling || CurrentPlayerMovementState == PlayerMovementState.Walking || CurrentPlayerMovementState == PlayerMovementState.Running || CurrentPlayerMovementState == PlayerMovementState.Sprinting;
+    }
+}
